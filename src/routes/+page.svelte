0d40f3eac3d0af05c4f8de3f410938827527106f
Bishal Dewansh <bishaldewansh@Bishals-MacBook-Air.local>
2023-11-09 09:23:29 +0530

    fifthcommits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,6 @@
   import Signup from './signup.svelte';
   import Login from './login/login.svelte';
   import Dashboard from './dashboard.svelte';
-
-  
   
 
   onMount(() => {
@@ -14,7 +12,86 @@
   });
 
   $: mypage = "LOADING";
-  
+  $: todos = [];    //$: To make it reactive 
+
+  //Fetching add-todo
+  let title = "";
+  $: console.log("Title: ",title);  
+  const handleAddTodoBtn = async() => {
+      fetch("https://todossd.vercel.app/api/add-todo", {
+          method:"POST",
+          body:JSON.stringify({
+              title,    //Inserting data into title = ""
+          }),
+      })
+      .then((res) => res.json())
+      .then((data) => {
+      console.log("Response Data:", data);
+      todos = [...todos, data];   //Adding data in todos = [] to display in Todo List
+      title = "";   //To show the placeholder as null in the input text
+    });
+  }
+
+
+  //Fetching get-todo
+  const getTodo = () => {
+  fetch("https://todossd.vercel.app/api/get-todo")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Data: ", data);
+      todos = data;   //inserting data into todos = []
+    });
+  };
+  getTodo();    //Calling Function
+
+
+  //Fetching delete-todo
+  const handleDeleteTodoBtn = (id) => {
+  fetch("https://todossd.vercel.app/api/delete-todo", {
+    method: "POST",
+    body: JSON.stringify({
+      id: id,
+    }),
+  })
+  .then((res) => res.json())
+  .then((data) => {
+      console.log("Data:", data);
+
+      //Disaapering data from Todo List 
+      let new_todos = todos.filter((item) => item._id !== id);
+      todos = new_todos;
+    });
+  };
+
+  let editTodoId = "";
+  const handleEditBtnClick = (id) => {
+  editTodoId = id;
+
+  let todo = todos.find((item) => item._id === id);
+  console.log("Todo: ", todo);
+  title = todo.title;   //After clicking on Edit data will move to input placeholder
+  };
+  const haneleEditSubmitBtnClick = () => {
+  fetch("https://todossd.vercel.app/api/edit-todo", {
+    method: "POST",
+    body: JSON.stringify({
+      id: editTodoId,
+      title: title,
+    }),
+  })
+  .then((res) => res.json())
+  .then((data) => {
+  console.log("Data: ", data);
+  todos = todos.map((item) => {
+      if (item._id === editTodoId) {
+          item.title = data.title;
+          }
+          return item;
+          });
+          title = "";
+          editTodoId = "";
+      });
+  };
   const handleStateChange=(data)=>{
     console.log(data);
     mypage=data
